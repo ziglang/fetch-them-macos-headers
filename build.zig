4920936c408d05eec93fa7a913b01d6b280cff2d
Jakub Konka <kubkon@jakubkonka.com>
2020-10-20 20:41:21 +0200

    Clean up creating subdirs

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("x86_64-macos-gnu", "src/main.zig");
+    const exe = b.addExecutable("fetch_them_macos_headers", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const fs = std.fs;
 const mem = std.mem;
 const tmpDir = std.testing.tmpDir;
 
@@ -9,17 +10,22 @@ pub fn main() !void {
     var tmp = tmpDir(.{});
     defer tmp.cleanup();
 
-    const tmp_out_file = try std.fs.path.join(alloc, &[_][]const u8{ "zig-cache", "tmp", tmp.sub_path[0..], "tmp" });
-    defer alloc.free(tmp_out_file);
+    const tmp_path = try tmp.dir.realpathAlloc(alloc, ".");
+    defer alloc.free(tmp_path);
 
-    const out_loc_list = try std.fs.path.join(alloc, &[_][]const u8{ "zig-cache", "tmp", tmp.sub_path[0..], "upgrade.o.d" });
-    defer alloc.free(out_loc_list);
+    const tmp_filename = "tmp";
+    const tmp_file_path = try fs.path.join(alloc, &[_][]const u8{ tmp_path, tmp_filename });
+    defer alloc.free(tmp_file_path);
+
+    const headers_list_filename = "upgrade.o.d";
+    const headers_list_path = try fs.path.join(alloc, &[_][]const u8{ tmp_path, headers_list_filename });
+    defer alloc.free(headers_list_path);
 
     // TODO instead of calling `cc` as a child process here,
     // hook in directly to `zig cc` API.
     const res = try std.ChildProcess.exec(.{
         .allocator = alloc,
-        .argv = &[_][]const u8{ "cc", "-o", tmp_out_file, "src/upgrade.c", "-MD", "-MV", "-MF", out_loc_list },
+        .argv = &[_][]const u8{ "cc", "-o", tmp_file_path, "src/upgrade.c", "-MD", "-MV", "-MF", headers_list_path },
     });
     defer {
         alloc.free(res.stdout);
@@ -27,35 +33,42 @@ pub fn main() !void {
     }
 
     // Read in the contents of `upgrade.o.d`
-    const loc_list_file = try tmp.dir.openFile("upgrade.o.d", .{});
-    defer loc_list_file.close();
+    const headers_list_file = try tmp.dir.openFile(headers_list_filename, .{});
+    defer headers_list_file.close();
 
     // Create out dir
-    var out_dir = try std.fs.cwd().makeOpenPath("x86_64-macos-gnu", .{});
-    defer out_dir.close();
+    var out_dir = try fs.cwd().makeOpenPath("x86_64-macos-gnu", .{});
+    var dirs = std.StringHashMap(fs.Dir).init(alloc);
+    defer dirs.deinit();
+    try dirs.putNoClobber(".", out_dir);
 
-    const loc_list_str = try loc_list_file.reader().readAllAlloc(alloc, std.math.maxInt(usize));
-    defer alloc.free(loc_list_str);
+    const headers_list_str = try headers_list_file.reader().readAllAlloc(alloc, std.math.maxInt(usize));
+    defer alloc.free(headers_list_str);
     const prefix = "/usr/include";
-    var it = mem.split(loc_list_str, "\n");
+    var it = mem.split(headers_list_str, "\n");
     while (it.next()) |line| {
         if (mem.lastIndexOf(u8, line, "clang") != null) continue;
         if (mem.lastIndexOf(u8, line, prefix[0..])) |idx| {
             const out_rel_path = line[idx + prefix.len + 1 ..];
             const out_rel_path_stripped = mem.trim(u8, out_rel_path, " \\");
-            var dir = if (std.fs.path.dirname(out_rel_path_stripped)) |dirname|
-                try out_dir.makeOpenPath(dirname, .{})
-            else
-                try std.fs.cwd().makeOpenPath("x86_64-macos-gnu", .{});
-            defer dir.close();
-            const basename = std.fs.path.basename(out_rel_path_stripped);
+            const dirname = fs.path.dirname(out_rel_path_stripped) orelse ".";
+            const maybe_dir = try dirs.getOrPut(dirname);
+            if (!maybe_dir.found_existing) {
+                maybe_dir.entry.value = try out_dir.makeOpenPath(dirname, .{});
+            }
+            const basename = fs.path.basename(out_rel_path_stripped);
 
             const line_stripped = mem.trim(u8, line, " \\");
-            const abs_dirname = std.fs.path.dirname(line_stripped).?;
-            var orig_subdir = try std.fs.cwd().openDir(abs_dirname, .{});
+            const abs_dirname = fs.path.dirname(line_stripped).?;
+            var orig_subdir = try fs.cwd().openDir(abs_dirname, .{});
             defer orig_subdir.close();
 
-            try orig_subdir.copyFile(basename, dir, basename, .{});
+            try orig_subdir.copyFile(basename, maybe_dir.entry.value, basename, .{});
         }
+    }
+
+    var dir_it = dirs.iterator();
+    while (dir_it.next()) |entry| {
+        entry.value.close();
     }
 }
